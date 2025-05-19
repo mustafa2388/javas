313e883697186e3e23e57f0dd63f3fdd5cb6fbb8
MustafaNoor <mustafanoor2388@gmail.com>
2025-05-19 21:00:01 +0500

    change 3

---
+++ variable.js
@@ -0,0 +1,12 @@
+const accountId = 112233
+let accountEmail = "noor123@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "Thall"
+
+accountEmail = "abs@.com"
+accountPassword = "12121212"
+accountCity = "Peshawar"
+
+console.table([accountEmail, accountPassword, accountCity])
+
