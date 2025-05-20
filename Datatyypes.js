8b0c03a9439945907aa9e62decdebeff3ddc6713
MustafaNoor <mustafanoor2388@gmail.com>
2025-05-20 15:13:04 +0500

    conversion

---
+++ Datatyypes.js
@@ -0,0 +1,14 @@
+let score = "55abs";
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let number = 33
+
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
