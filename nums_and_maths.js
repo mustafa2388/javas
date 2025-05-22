8a5a933ee4824cf98fc98653bd504f2d42542baa
MustafaNoor <mustafanoor2388@gmail.com>
2025-05-22 16:56:36 +0500

    nums and math

---
+++ nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 500
+//console.log(score)
+
+const balance = new Number(200)
+
+//console.log(balance)
+
+//console.log(balance.toString())
+
+
+//console.log(balance.toFixed(2))
+
+const num = 10.123456789
+//console.log(num.toPrecision(2))
+
+const hunderds = 10000000
+//console.log(hunderds.toLocaleString(en - PAK))
+
+
+
+// ************Maths*********
+
+// console.log(Math)
+// console.log(Math.abs(-10))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.5))
+// console.log(Math.max(45, 33, 44, 66))
+// console.log(Math.min(55, 44, 33, 22))
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
