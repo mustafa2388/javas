6bc217604e14866df719c0a3c3f118560cd6261c
MustafaNoor <mustafanoor2388@gmail.com>
2025-05-26 15:07:09 +0500

    functions

--- functions.js
+++ functions.js
@@ -1,12 +1,12 @@
-// function myName() {
-//     console.log("Mustafa")
-// }
-// myName()
+function myName() {
+    console.log("Mustafa")
+}
+myName()
 
-// function addTwoNumber(number1, number2) {
-//     console.log(number1 + number2)
-// }
-// addTwoNumber(5, 10)
+function addTwoNumber(number1, number2) {
+    console.log(number1 + number2)
+}
+addTwoNumber(5, 10)
 
 function addTwoNumber(number1, number2) {
     // let result = number1 + number2
@@ -14,7 +14,7 @@ function addTwoNumber(number1, number2) {
     return number1 + number2
 }
 const result = addTwoNumber(5, 10)
-// console.log("Result:", result)
+console.log("Result:", result)
 
 function loginUseerMessage(username = "Noor") {
     if (!username) {
@@ -25,13 +25,13 @@ function loginUseerMessage(username = "Noor") {
     return `${username} you just logged in`
 }
 
-// console.log(loginUseerMessage("Mustafa"))
+console.log(loginUseerMessage("Mustafa"))
 
-// function calculateCartPricee(val1, val2, ...num1) {
-//     return num1
+function calculateCartPricee(val1, val2, ...num1) {
+    return num1
 
-// }
-// console.log(calculateCartPricee(200, 300, 4000, 5000))
+}
+console.log(calculateCartPricee(200, 300, 4000, 5000))
 
 const user = {
     username: "Mustafa",
@@ -39,10 +39,10 @@ const user = {
 }
 
 function handleObject(anobject) {
-    // console.log(`Username is ${anobject.username} and the price is ${anobject.price}`)
+    console.log(`Username is ${anobject.username} and the price is ${anobject.price}`)
 
 }
-// handleObject(user)
+handleObject(user)
 
 handleObject({
     username: "Khan",
