6de6fb7197efbdfdfe6a54220f7596601b497d13
MustafaNoor <mustafanoor2388@gmail.com>
2025-05-27 01:37:45 +0500

    if,ifelse,nestedifelse

---
+++ control-flow.js
@@ -0,0 +1,31 @@
+// const isloggedIn = true
+// const temp = 50
+// if (temp === 40) {
+//     console.log("Its too hot")
+// }
+// else {
+//     console.log("nnormal")
+// }
+
+// const score = 200
+
+// if (score >= 100) {
+//     const power = "Fly"
+//     console.log(`You have the power to ${power}`)
+// }
+// console.log(`User power is ${power}`) // the error is because of var This will throw an error because 'power' is not defined in this scope
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const userLoggedInFromGoogle = false
+// const userLoggedInFromEmail = true
+
+// if (userLoggedIn && debitCard) { // Logical AND operator checks if both conditions are true then it executes the block if not it will not execute the block
+//     console.log("You can buy the product")
+// }
+
+// if (userLoggedInFromGoogle || userLoggedInFromEmail) { // Logical OR operator checks if at least one condition is true then it executes the block
+//     console.log("User logged in from Google or Email")
+// }
+
