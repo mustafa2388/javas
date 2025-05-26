aa51e09dfc51d760f4e9d473522bc17ba5723d53
MustafaNoor <mustafanoor2388@gmail.com>
2025-05-26 16:04:17 +0500

    diff ways of functiion

--- scope.js
+++ scope.js
@@ -1,7 +1,7 @@
 let a = 300
 if (true) {
     let a = 400
-    console.log("Inside if block:", a) // 400
+    // console.log("Inside if block:", a) // 400
 }
 //console.log(a)
 
@@ -14,7 +14,7 @@ function one() {
 
     function two() {
         const website = "youtube"
-        console.log(username) // Mustafa
+        // console.log(username) // Mustafa
     }
     //console.log(website)
 
@@ -27,7 +27,22 @@ if (true) {
     username = "Mustafa"
     if (username === "Mustafa") {
         const website = " youtube"
-        console.log(username + website) // youtube
+        // console.log(username + website) // youtube
     }
     //console.log(username) // Mustafa
 }
+// ********************INTRESTING***********************
+
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+
+// console.log(addtwo(4)) // ReferenceError: Cannot access 'addtwo' before initialization
+// This will throw an error because addtwo is not hoisted
+
+
+const addtwo = function (num) {
+    return num + 2
+}
+console.log(addtwo(7))  // This will work because addtwo is called after its declaration
