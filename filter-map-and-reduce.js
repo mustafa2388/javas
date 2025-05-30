7ea9af59c9821519aa0b28dfeb863f49f1445408
MustafaNoor <mustafanoor2388@gmail.com>
2025-05-30 16:35:39 +0500

    fel

---
+++ filter-map-and-reduce.js
@@ -0,0 +1,12 @@
+// const coding = ["python", "js", "swift", "Cpp", "Java"]
+
+// const values = coding.forEach((item) => {
+//     // console.log(item)
+//     return item
+// })
+// console.log(values)
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter((num) => num > 4)
+console.log(newNums)
